ae1e667cbb2227139f884061daf78d5ec1d49870
Patricio Bruna <pbruna@gmail.com>
2013-05-22 18:08:16 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Folio::Application.config.secret_key_base = 'b818d7c13c6dd8da7e35b193635bed28535f011e29e931eaac2dcf310b12f2145e2cc5b7539ce2077058cd7150c1c1636d3e31f48a6b04dcb356e7068b1db9d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Folio::Application.config.session_store :cookie_store, key: '_folio_session'
