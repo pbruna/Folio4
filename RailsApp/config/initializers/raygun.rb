e093bee7207e83e81bfebdc5df54d850d6fb95dd
Patricio Bruna <pbruna@gmail.com>
2014-08-03 19:17:10 -0400

    Cambio directorio Raiz

--- RailsApp/config/initializers/raygun.rb
+++ RailsApp/config/initializers/raygun.rb
@@ -1,6 +1,6 @@
 Raygun.setup do |config|
   config.api_key = "DLHqcHjVC95yxCe/z8rz5Q=="
   config.filter_parameters = Rails.application.config.filter_parameters
-  config.enable_reporting = true
+  config.enable_reporting = Rails.env.development?
   config.silence_reporting = false
 end
