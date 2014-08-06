916c5a15ec5fccfc85c9025cb85367beaf5bf2d4
Gabriel Yañez <gabrielyanez@MacBook-Pro-de-Gabriel.local>
2014-08-06 13:15:48 -0400

    Cambio de Logo

--- RailsApp/config/initializers/airbrake.rb
+++ RailsApp/config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
   config.api_key = 'ef7f469c86f8d8bd03c34713ee382bad'
-  config.development_environments = []
+  config.development_environments = [:development]
 end
