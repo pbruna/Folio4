4f8c63ae1488d8d6c542e780301c4e08a4b078f9
Patricio Bruna <pbruna@gmail.com>
2014-08-11 17:26:42 -0400

    Preparando paso a produccion

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,12 @@ MAINTAINER Patricio Bruna <pbruna@itlinux.cl>
 
 ENV RAILS_ENV production
 
+RUN rake db:setup
 RUN rake db:migrate
 RUN rake db:seed
 RUN rake assets:precompile
 RUN rake assets:sync
 RUN rake tmp:create
 RUN rake tmp:clear
+
+EXPOSE 8080:8080
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,6 @@ Folio::Application.routes.draw do
     root :to => "accounts#dashboard"
   end
   
-  resources :accounts, :only => [:new, :create] , :constraints => { :subdomain => /www/ }
+  resources :accounts, :only => [:new, :create] , :constraints => { :subdomain => /www|app/ }
   
 end
