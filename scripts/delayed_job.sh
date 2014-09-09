f4d769fc78c7009bf9e3dfc3d31ec1f601c28197
Patricio Bruna <pbruna@gmail.com>
2014-09-09 06:53:42 -0300

    Corregido delayed_job script start

--- scripts/delayed_job.sh
+++ scripts/delayed_job.sh
@@ -3,4 +3,4 @@
 # `chpst` is part of running. `chpst -u app` runs the given command
 # as the user `app`. If you omit this, the command will be run as root.
 cd /home/app/folio4
-exec chpst -u app bin/delayed_job -e production -n 4 >>/var/log/delayed_job.log 2>&1
+exec chpst -u app bin/delayed_job -e production -n 4 start >>/var/log/delayed_job.log 2>&1
