b97bb9f02356eca19bcca207d4e67204fa850552
Patricio Bruna <pbruna@gmail.com>
2014-12-18 17:00:09 -0300

    Correcion Dockerfile: Memcache y Delayed Job

--- scripts/delayed_job.sh
+++ scripts/delayed_job.sh
@@ -1,6 +1,3 @@
-### In delayed_job.sh (make sure this file is chmod +x):
 #!/bin/sh
-# `chpst` is part of running. `chpst -u app` runs the given command
-# as the user `app`. If you omit this, the command will be run as root.
-cd /home/app/folio4
-exec chpst -u app rake jobs:work RAILS_ENV=production >>/var/log/delayed_job.log 2>&1
+
+exec su app -c "cd /home/app/folio4 && rake jobs:work RAILS_ENV=production >>/var/log/delayed_job.log 2>&1"
